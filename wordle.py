f38798165ed738259308eff73b2787cf831cf93e
Yao21516 <love040102@example.com>
2022-10-14 17:37:48 +0800

    基本的題目要求

--- wordle.py
+++ wordle.py
@@ -2,3 +2,11 @@ if __name__ == "__main__":
     user_input = input()
 
     answer = "apple"
+
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")
